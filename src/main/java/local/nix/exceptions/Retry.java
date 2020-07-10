95ff4f10e2c377383982bd458da23ee8a6870057
rs <rs@MacBook-Air-rs.local>
2020-07-10 09:13:14 +0300

    Retry.java WAS CHANGED. repeatBlock METHOD WAS CHANGED

--- src/main/java/local/nix/exceptions/Retry.java
+++ src/main/java/local/nix/exceptions/Retry.java
@@ -6,22 +6,23 @@ public class Retry {
 
     public void repeatBlock(int i, Block block) throws Exception{
 
+        Thread.sleep(100 * sleep++);
 
         if(1 == i) {
             try {
-                Thread.sleep(100 * sleep);
                 block.run();
             } catch (Exception e1) {
                 throw e1;
             }
+        } else {
+
+            try {
+                block.run();
+            } catch (Exception e) {
+                repeatBlock(i-1, block);
+            }
         }
 
-        try {
-            Thread.sleep(100 * sleep++);
-            block.run();
-        } catch (Exception e) {
-            repeatBlock(i-1, block);
-        }
 
 
     }
